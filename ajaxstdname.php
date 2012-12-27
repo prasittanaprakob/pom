82861cb154bc635d170693a2b3a0a4f4def62b5a
Prasit Tanaprakob <prasittanaprakob@gmail.com>
2012-12-27 18:29:35 +0700

    newly deployed

---
+++ ajaxstdname.php
@@ -0,0 +1,30 @@
+<?php
+$host="localhost";
+$databasename="prasitcom_ssru";
+$username="prasitcom_admin";
+$password="123456";
+
+$studentid = $_GET['xtemp'];
+$con=mysql_connect($host,$username,$password) or die (mysql_error());
+mysql_select_db($databasename) or die (mysql_error());
+$query="select * from user where username='$studentid'";
+		mysql_query("SET character_set_results=utf8");
+		mysql_query("SET character_set_client=utf8");
+		mysql_query("SET character_set_connection=utf8");
+$results=mysql_query($query);
+
+if (mysql_error()) {
+//	header ("HTTP/1.1 500 Internal Server Error");
+	echo $query . '\n';
+	echo mysql_error();
+}
+else
+{
+	$rows=array();
+	while ($r=mysql_fetch_assoc($results)) {
+		$rows[] = $r;
+	}
+	echo json_encode($rows);
+}
+?>
+				
